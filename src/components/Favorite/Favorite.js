383a542fdc252a72769469bb11bfba0cb6a9f3fd
Rushil dhinoja <mr.rushil17@gmail.com>
2021-07-30 18:34:18 +0530

    Test build

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -5,51 +5,54 @@ import { getLiked } from '../../actions/liked';
 import { connect } from 'react-redux';
 import Loader from '../layout/Loader';
 import PokemonItem from '../pokemons/PokemonItem';
-
+// Test
 const Favorite = ({ history, getLiked, liked: { liked, loading } }) => {
-    const [exists, setExists] = useState(false);
+  const [exists, setExists] = useState(false);
 
-    useEffect(() => {
-        localStorage.getItem('liked') && JSON.parse(localStorage.getItem('liked')).length !== 0
-            ? setExists(true)
-            : setExists(false);
-        getLiked();
-    }, [getLiked]);
+  useEffect(() => {
+    localStorage.getItem('liked') &&
+    JSON.parse(localStorage.getItem('liked')).length !== 0
+      ? setExists(true)
+      : setExists(false);
+    getLiked();
+  }, [getLiked]);
 
-    return (
-        <div className='container'>
-            <div className='favorite-container'>
-                {loading ? (
-                    <Loader />
-                ) : exists && liked.length !== 0 ? (
-                    <div className='favorite-container__liked'>
-                        {' '}
-                        <h2>Your Liked Pokemons</h2>{' '}
-                        <div className='pokemon__grid'>
-                            {liked.map((like, index) => (
-                                <PokemonItem key={index} pokemon={like.data} />
-                            ))}
-                        </div>
-                    </div>
-                ) : (
-                    <div className='favorite-container__error'>
-                        {' '}
-                        <h2>You don't have any liked pokemons</h2>
-                        <button onClick={() => history.push('/pokemons')}>Go Like Some</button>{' '}
-                    </div>
-                )}
+  return (
+    <div className='container'>
+      <div className='favorite-container'>
+        {loading ? (
+          <Loader />
+        ) : exists && liked.length !== 0 ? (
+          <div className='favorite-container__liked'>
+            {' '}
+            <h2>Your Liked Pokemons</h2>{' '}
+            <div className='pokemon__grid'>
+              {liked.map((like, index) => (
+                <PokemonItem key={index} pokemon={like.data} />
+              ))}
             </div>
-        </div>
-    );
+          </div>
+        ) : (
+          <div className='favorite-container__error'>
+            {' '}
+            <h2>You don't have any liked pokemons</h2>
+            <button onClick={() => history.push('/pokemons')}>
+              Go Like Some
+            </button>{' '}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 Favorite.propTypes = {
-    liked: PropTypes.object.isRequired,
-    getLiked: PropTypes.func.isRequired,
+  liked: PropTypes.object.isRequired,
+  getLiked: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-    liked: state.liked,
+  liked: state.liked,
 });
 
 export default connect(mapStateToProps, { getLiked })(withRouter(Favorite));
